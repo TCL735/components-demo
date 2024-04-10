c0b274f81e3ab4c364f4a6494c0babe959485177
Timmy Luong <TCL735@users.noreply.github.com>
2024-04-10 13:50:29 -0700

    feat: update overall appearance and styling

--- src/components/Sections.tsx
+++ src/components/Sections.tsx
@@ -35,6 +35,14 @@ export enum GroupName {
   Settings = "Settings",
 }
 
+enum IconColor {
+  Data = "blue",
+  Security = "purple",
+  Store = "grey",
+  Tools = "lightblue",
+  Settings = "lightgray",
+}
+
 export interface Group {
   icon: FC;
   name: GroupName;
@@ -42,6 +50,7 @@ export interface Group {
 
 export interface Section {
   icon: FC;
+  iconColor: IconColor;
   group: GroupName;
   name: string;
   description: string;
@@ -73,24 +82,28 @@ export const groups: Array<Group> = [
 export const sections: Array<Section> = [
   {
     icon: IconFolderOpen,
+    iconColor: IconColor.Data,
     group: GroupName.Data,
     name: "Catalog",
     description: "Description of application",
   },
   {
     icon: IconEye,
+    iconColor: IconColor.Data,
     group: GroupName.Data,
     name: "Observability",
     description: "Description of application",
   },
   {
     icon: IconTournament,
+    iconColor: IconColor.Data,
     group: GroupName.Data,
     name: "Lineage",
     description: "Description of application",
   },
   {
     icon: IconSwitch3,
+    iconColor: IconColor.Data,
     group: GroupName.Data,
     name: "ETL",
     description: "Description of application",
@@ -98,24 +111,28 @@ export const sections: Array<Section> = [
 
   {
     icon: IconUserCheck,
+    iconColor: IconColor.Security,
     group: GroupName.Security,
     name: "Access",
     description: "Description of application",
   },
   {
     icon: IconAlertTriangle,
+    iconColor: IconColor.Security,
     group: GroupName.Security,
     name: "Logs",
     description: "Description of application",
   },
   {
     icon: IconCompass,
+    iconColor: IconColor.Security,
     group: GroupName.Security,
     name: "Discovery",
     description: "Description of application",
   },
   {
     icon: IconFileDescription,
+    iconColor: IconColor.Security,
     group: GroupName.Security,
     name: "Policies",
     description: "Description of application",
@@ -123,42 +140,49 @@ export const sections: Array<Section> = [
 
   {
     icon: IconBrandAws,
+    iconColor: IconColor.Store,
     group: GroupName.Store,
     name: "AWS",
     description: "Description of application",
   },
   {
     icon: IconDog,
+    iconColor: IconColor.Store,
     group: GroupName.Store,
     name: "Datadog",
     description: "Description of application",
   },
   {
     icon: IconSquareLetterS,
+    iconColor: IconColor.Store,
     group: GroupName.Store,
     name: "Splunk",
     description: "Description of application",
   },
   {
     icon: IconHexagonNumber3,
+    iconColor: IconColor.Store,
     group: GroupName.Store,
     name: "S3",
     description: "Description of application",
   },
   {
     icon: IconBrandGithub,
+    iconColor: IconColor.Store,
     group: GroupName.Store,
     name: "Github",
     description: "Description of application",
   },
   {
     icon: IconTelescope,
+    iconColor: IconColor.Store,
     group: GroupName.Store,
     name: "datahub",
     description: "Description of application",
   },
   {
     icon: IconPlus,
+    iconColor: IconColor.Store,
     group: GroupName.Store,
     name: "Manage Store apps",
     description: "Description of application",
@@ -166,18 +190,21 @@ export const sections: Array<Section> = [
 
   {
     icon: IconKey,
+    iconColor: IconColor.Tools,
     group: GroupName.Tools,
     name: "API Keys",
     description: "Description of application",
   },
   {
     icon: IconSum,
+    iconColor: IconColor.Tools,
     group: GroupName.Tools,
     name: "Sigma",
     description: "Description of application",
   },
   {
     icon: IconBinaryTree,
+    iconColor: IconColor.Tools,
     group: GroupName.Tools,
     name: "Workflow",
     description: "Description of application",
@@ -185,12 +212,14 @@ export const sections: Array<Section> = [
 
   {
     icon: IconSettingsBolt,
+    iconColor: IconColor.Settings,
     group: GroupName.Settings,
     name: "Common Setting A",
     description: "Description of application",
   },
   {
     icon: IconSettingsDollar,
+    iconColor: IconColor.Settings,
     group: GroupName.Settings,
     name: "Special Setting B",
     description: "Description of application",
