8e2b51cfa0974d89b9fc084110d2cfad96d2e2bc
Timmy Luong <TCL735@users.noreply.github.com>
2024-04-10 18:56:20 -0700

    feat: add navigation buttons (stubs)

--- src/components/Contents.tsx
+++ src/components/Contents.tsx
@@ -1,93 +1,137 @@
-import React, { FC, forwardRef, useMemo } from "react";
+import React, { FC, createRef, forwardRef, useMemo, useRef } from "react";
 import {
   Box,
   Button,
   Grid,
   InputAdornment,
-  Stack,
   TextField,
   Typography,
 } from "@mui/material";
 import { IconSearch } from "@tabler/icons-react";
-import { GroupName, groups, sections } from "./Sections";
+import { GroupName, Group, sections } from "./Sections";
 import "./Contents.css";
 
-interface ContenGroupProps {
-  groupName: GroupName;
+interface ContentNavigationProps {
+  groups: Array<Group>;
+  refs: React.MutableRefObject<any[]>;
 }
-export const ContentGroup: FC<ContenGroupProps> = ({ groupName }) => {
-  const sectionRows = useMemo(
-    () => sections.filter((section) => section.group === groupName),
-    [groupName],
-  );
+
+export const ContentNavigation: FC<ContentNavigationProps> = ({
+  groups,
+  refs,
+}) => {
+  const handleClick = (index: number) => {
+    refs?.current?.[index]?.current.scrollIntoView();
+  };
 
   return (
-    <Grid item xs={12}>
-      <Typography variant="h6" id="group-name">
-        {groupName}
-      </Typography>
-      <Stack>
-        {sectionRows.map((section) => {
-          const Icon = section.icon;
-          return (
-            <Box key={section.name} className="section-row">
-              <Box
-                className="section-icon-container"
-                style={{ backgroundColor: section.iconColor }}
-              >
-                <Icon />
-              </Box>
-              <Typography variant="h6" className="section-name">
-                {section.name}
-              </Typography>
-              <Typography className="section-description">
-                {section.description}
-              </Typography>
-              <Button
-                variant="outlined"
-                id="section-button-shortcut"
-                size="small"
-              >
-                <span>{section.shortcut}</span>
-              </Button>
-            </Box>
-          );
-        })}
-      </Stack>
-    </Grid>
+    <Box className="navigation-bar">
+      <Button className="navigation-button">All</Button>
+      {groups.map((group, index) => {
+        const Icon = group.icon;
+        return (
+          <Button
+            key={group.name}
+            className="navigation-button"
+            onClick={() => handleClick(index)}
+          >
+            <Icon />
+            {group.name}
+          </Button>
+        );
+      })}
+    </Box>
   );
 };
 
-export const Contents = forwardRef((props, ref) => {
+interface ContentGroupProps {
+  groupName: GroupName;
+}
+export const ContentGroup = forwardRef(
+  ({ groupName }: ContentGroupProps, ref) => {
+    const sectionRows = useMemo(
+      () => sections.filter((section) => section.group === groupName),
+      [groupName],
+    );
+
+    return (
+      <Grid item xs={12}>
+        <Typography variant="h6" id="group-name">
+          {groupName}
+        </Typography>
+        <Box ref={ref}>
+          {sectionRows.map((section) => {
+            const Icon = section.icon;
+            return (
+              <Box key={section.name} className="section-row">
+                <Box
+                  className="section-icon-container"
+                  style={{ backgroundColor: section.iconColor }}
+                >
+                  <Icon />
+                </Box>
+                <Typography variant="h6" className="section-name">
+                  {section.name}
+                </Typography>
+                <Typography className="section-description">
+                  {section.description}
+                </Typography>
+                <Button
+                  variant="outlined"
+                  id="section-button-shortcut"
+                  size="small"
+                >
+                  <span>{section.shortcut}</span>
+                </Button>
+              </Box>
+            );
+          })}
+        </Box>
+      </Grid>
+    );
+  },
+);
+
+export const Contents = forwardRef((props: { groups: Array<Group> }, ref) => {
+  const { groups } = props;
+  const groupRefs = useRef(Array.from(groups, () => createRef()));
+
   return (
     <Box id="contents" mx={10} my={5} ref={ref}>
-      <Box className="contents-search">
-        <TextField
-          className="textfield-search"
-          autoFocus
-          fullWidth
-          placeholder="Find info, Ask questions or Run queries"
-          InputProps={{
-            className: "textfield-search-input",
-            startAdornment: (
-              <InputAdornment position="start">
-                <IconSearch />
-              </InputAdornment>
-            ),
-            endAdornment: (
-              <InputAdornment position="end">
-                <Button id="button-command-e" variant="outlined" size="large">
-                  <span>'/' for commands</span>
-                </Button>
-              </InputAdornment>
-            ),
-          }}
-        />
+      <Box className="header">
+        <Box className="contents-search">
+          <TextField
+            className="textfield-search"
+            autoFocus
+            fullWidth
+            placeholder="Find info, Ask questions or Run queries"
+            InputProps={{
+              className: "textfield-search-input",
+              startAdornment: (
+                <InputAdornment position="start">
+                  <IconSearch />
+                </InputAdornment>
+              ),
+              endAdornment: (
+                <InputAdornment position="end">
+                  <Button id="button-command-e" variant="outlined" size="large">
+                    <span>'/' for commands</span>
+                  </Button>
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Box>
+        <ContentNavigation groups={groups} refs={groupRefs} />
       </Box>
 
-      <Grid container rowSpacing={2}>
-        {groups.map((group) => (
-          <ContentGroup key={group.name} groupName={group.name} />
+      <Grid container rowSpacing={2} className="content-body">
+        {groups.map((group, index) => (
+          <ContentGroup
+            key={group.name}
+            groupName={group.name}
+            ref={groupRefs.current?.[index]}
+          />
         ))}
       </Grid>
     </Box>
